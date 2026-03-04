11cf2070713c550c132116ac88dcdb47e84d4465
KAIQUE CARVALHO COSTA <kaique.carvalho.costa8@gmail.com>
2026-03-04 16:50:28 -0300

    Adição do desafio

--- Atividades/ATIVIDADES-1-A-5/atividade5/modulo/calculo.js
+++ Atividades/ATIVIDADES-1-A-5/atividade5/modulo/calculo.js
@@ -1,18 +1,18 @@
-function validacao (n1, n2, n3, n4){
+function validacao (n3, n4, escolha){
 
-    let valor1 = Number(n1)
-    let valor2 = Number(n2)
     let valor3 = Number(n3)
     let valor4 = Number(n4)
+    let opcao = Number(escolha)
 
-    if(isNaN(valor3) ||  isNaN(valor4))
+
+    if(isNaN(valor3) ||  isNaN(valor4) || isNaN(opcao))
     {
-        console.log ('ERRO: os dados recebidos contem letras escrva novamente com números')
+        console.log ('ERRO: os dados recebidos contem letras escreva novamente com números')
         process.exit(0)
     } else if (valor3 >= valor4){
         console.log ('ERRO: com os dados recebidos não sera possivel realizar a contagem')
         process.exit(0)
-    } else if (valor3 < 0 || valor3 > 500 || valor4 <1 || valor4 >500){
+    } else if (valor3 < 0 || valor3 > 500 || valor4 <1 || valor4 >500 || opcao <=0 || opcao >=3){
         console.log ('ERRO: com os dados recebidos não sera possivel realizar a contagem')
         process.exit(0)
     }
@@ -20,28 +20,51 @@ function validacao (n1, n2, n3, n4){
 
 }
 
-function calcularTabuada (valor3, valor4){
+function calcularTabuada (valor3, valor4, escolha){
 
     let listaPares = new Set()
     let listaImpares = new Set()
+    let opcao = escolha
     
     let contInicial = Number(valor3)
     let contFinal   = Number(valor4)
     
         while(contInicial <= contFinal){
             console.log(contInicial)
-            contInicial++
-            if (resultado %2 ===0){
-                listaPares.add(resultado)
-            } if (resultado %2 !== 0){
-                listaImpares.add(resultado)
+            if (contInicial % 2 === 0) {
+                listaPares.add(contInicial)
+            } else {
+                listaImpares.add(contInicial)
             }
+            contInicial++
+            
         }
-        console.log('Números pares encontrados:')
-        console.log(listaPares)
-        console.log('-------------------------------------------------------------')
-        console.log('Números ipares encontrados:')
-        console.log(listaImpares)
+        if (opcao == 1) {
+            console.log('--------------------------------------------')
+            console.log('Números pares:')
+            console.log('--------------------------------------------')
+            console.log(listaPares)
+            console.log('--------------------------------------------')
+    
+        } else if (opcao == 2) {
+            console.log('--------------------------------------------')
+            console.log('Números ímpares:')
+            console.log('--------------------------------------------')
+            console.log(listaImpares)
+            console.log('--------------------------------------------')
+    
+        } else {
+            console.log('--------------------------------------------')
+            console.log('Números pares:')
+            console.log('--------------------------------------------')
+            console.log(listaPares)
+            console.log('--------------------------------------------')
+            console.log('Números ímpares:')
+            console.log('--------------------------------------------')
+            console.log(listaImpares)
+            console.log('--------------------------------------------')
+        }
+    
         process.exit(0)
     }
        
